e8e425c749d2f348994b6dfdf2aa7b680766e37b
Ersin Aka <ersinaka37@gmail.com>
2022-11-02 14:52:39 +0300

    Placeholder changed

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
         <input
           onChange={(e) => setSearch(e.target.value)}
           type="text"
-          placeholder="Placeholder"
+          placeholder="Search city"
           className=" my-5 px-3 w-[400px] py-3 placeholder-blueGray-300 text-blueGray-600   bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring "
         />
         {city && <City city={city} />}
